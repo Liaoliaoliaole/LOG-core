a675da54f9fc151447905f36d3eca23445f668ee
Sam Harry Tzavaras <fantomsam@hotmail.com>
2019-11-18 19:13:51 +0200

    Update

--- src/morfeas_SDAQ_if.c
+++ src/morfeas_SDAQ_if.c
@@ -39,26 +39,32 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //Include SDAQ Driver header
 #include "sdaq-worker/src/SDAQ_drv.h"
 //Include Functions implementation header
-#include "sdaq-worker/src/Modes.h"
+//#include "sdaq-worker/src/Modes.h"
 
 //Morfeas_SDAQ-if struct
 struct Morfeas_SDAQ_if_stats{
 	char *CAN_IF_name;
 	float Bus_util;
 	unsigned char detected_SDAQs;
-	unsigned char online_SDAQs;
+	GSList *list_SDAQs;
 };
 
+struct SDAQ_info_entry{
+	sdaq_status entry_status;
+	sdaq_info entry_info;
+};
 
 //Global variables
 struct timespec tstart;
 int socket_num, run=1;
 
-//Local function (declaration)
+/*Local function (declaration)*/
 void CAN_if_timer_handler(int signum);
 void quit_signal_handler(int signum);
 void print_usage(char *prog_name);//print the usage manual
-
+int SDAQs_discover(int socket_fd, struct Morfeas_SDAQ_if_stats *stats);//discover the online SDAQs and load them on stats
+//lists related functions
+void free_SDAQ_info_entry(gpointer node);//used with g_slist_free_full to free the data of each node
 
 int main(int argc, char *argv[])
 {
@@ -68,7 +74,8 @@ int main(int argc, char *argv[])
 	struct sockaddr_can addr;	
 	struct can_filter RX_filter;
 	sdaq_can_id *can_filter_enc;
-	
+	//data_and stats of the Morfeas-SDAQ_IF
+	struct Morfeas_SDAQ_if_stats stats = {0};
 	//Timers related Variables
 	struct itimerval timer;	
 	
@@ -119,11 +126,16 @@ int main(int argc, char *argv[])
 		perror("Error in socket bind");
 		exit(EXIT_FAILURE);
 	}
-	
 	//Link signal SIGALRM to timer's handler
 	signal(SIGALRM, CAN_if_timer_handler);
 	//Link signal SIGINT to quit_signal_handler
 	signal(SIGINT, quit_signal_handler);
+	
+		//Actions on the bus 
+	//Stop any measuring activity on the bus
+	Stop(socket_num,Broadcast);
+	SDAQs_discover(socket_num, &stats);
+	
 	//Initialize timer expired time 
 	memset (&timer, 0, sizeof(struct itimerval));
 	timer.it_interval.tv_sec = 10;
@@ -140,8 +152,9 @@ int main(int argc, char *argv[])
 		sleep(1);
 	}
 	
-	close(socket_num);
+	g_slist_free_full(stats.list_SDAQs, free_SDAQ_info_entry);
 	printf("\n%s quiting...\n",argv[0]);
+	close(socket_num);
 	return EXIT_SUCCESS;
 }
 
@@ -168,7 +181,6 @@ void CAN_if_timer_handler (int signum)
 	return;
 }
 
-
 void print_usage(char *prog_name)
 {
 	const char preamp[] = {
@@ -183,4 +195,110 @@ void print_usage(char *prog_name)
 	printf("%s\nUsage: %s CAN-IF \n\n%s\n", preamp, prog_name,exp);
 	return;
 }
+
+//Lists related function implementation 
+struct SDAQ_info_entry* new_SDAQ_info_entry()
+{
+    struct SDAQ_info_entry *new_SDAQ = (struct SDAQ_info_entry *) g_slice_alloc(sizeof(struct SDAQ_info_entry));
+    return new_SDAQ;
+}
+
+void free_SDAQ_info_entry(gpointer SDAQentry_node) 
+{
+    g_slice_free(struct SDAQ_info_entry, SDAQentry_node);
+}
+   
+/*
+	Comparing function used in g_slist_find_custom, 
+	Controlled by target switch variable. 
+*/
+gint SDAQentry_find (gconstpointer node, gconstpointer arg)
+{
+	const unsigned int *arg_t = arg;
+	struct SDAQ_info_entry *node_dec = (struct SDAQ_info_entry *) node;
+	return node_dec->entry_status.dev_sn == (unsigned int) *arg_t ? 0 : 1; 
+}
+
+/*return a list with all the SDAQs on bus, sort by address*/
+GSList * find_SDAQs(int socket_fd)
+{
+	//internal List with SDAQs
+	GSList *ret_list=NULL;
+	
+	//CAN Socket and SDAQ related variables
+	struct can_frame frame_rx;
+	int RX_bytes;
+	sdaq_can_id *id_dec = (sdaq_can_id *)&(frame_rx.can_id);
+	sdaq_status *status_dec = (sdaq_status *)(frame_rx.data);
+	sdaq_info *info_dec = (sdaq_info *)(frame_rx.data);
+	//Time related Variables
+	time_t proc_start, proc_after;
+	//Variables for select
+	int retval;
+	struct timeval tv;
+	fd_set ready_for_read;
+	
+	//take time before start
+	time(&proc_start);
+	//Query device info from every device
+	QueryDeviceInfo(socket_fd,Broadcast);
+	do{
+		/* Set Watch SocketCAN to see when it's available for reading. */
+		FD_ZERO(&ready_for_read); //init ready_for_read
+		FD_SET(socket_fd, &ready_for_read); //link Socket_num with ready_for_read
+		tv.tv_sec = 0;
+		tv.tv_usec = 100000;//100ms	
+		//wait socket_num to be ready for read, or expired after timeout
+		retval = select(socket_fd+1, &ready_for_read, NULL, NULL, &tv);
+		if(retval == -1)
+		{
+			perror("select()");
+			close(socket_num);
+			exit(EXIT_FAILURE);
+		}
+		else if (retval)
+		{
+			RX_bytes = read(socket_fd, &frame_rx, sizeof(frame_rx));
+			if(RX_bytes==sizeof(frame_rx))
+			{
+				switch(id_dec->payload_type)
+				{
+					case Device_status:	
+						// check if node with same Serial number exist in the list. if no, do store.
+						if(g_slist_find_custom(ret_list,(gconstpointer)&(status_dec->dev_sn),SDAQentry_find)==NULL)  
+						{ 
+							struct SDAQ_info_entry *new_sdaq = new_SDAQ_info_entry();
+							if (new_sdaq) 
+							{
+								/*
+								// set SDAQ info data
+								new_sdaq->serial_number = status_dec->dev_sn;
+								new_sdaq->address = id_dec->device_addr; 
+								new_sdaq->dev_type = dev_type_str[status_dec->dev_type];
+								//printf("New SDAQ discovered with %d at address : %d\n",new_sdaq->serial_number,new_sdaq->address);
+								ret_list = g_slist_insert_sorted(ret_list, new_sdaq, SDAQentry_cmp);
+								*/
+							} 
+							else
+							{
+								fprintf(stderr,"Memory error\n");
+								exit(EXIT_FAILURE);
+							}
+						}
+						break;
+				}
+			}
+		}
+		//take time after scan process
+		time(&proc_after);
+	}while((proc_after - proc_start) < 3); //work for 3 seconds
+	return (GSList *) ret_list;
+}
+
+//
+int SDAQs_discover(int socket_fd, struct Morfeas_SDAQ_if_stats *stats)
+{
+	return 0;
+}
+
 
