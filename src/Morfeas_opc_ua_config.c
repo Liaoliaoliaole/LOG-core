135aa8220a4d53461c83fe249cc54d5fc614c3c8
Sam Harry Tzavaras <fantomsam@hotmail.com>
2019-12-14 20:58:02 +0200

    Update

--- src/Morfeas_opc_ua_config.c
+++ src/Morfeas_opc_ua_config.c
@@ -24,10 +24,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <open62541/plugin/securitypolicy_default.h>
 #include <open62541/server_config_default.h>
 
-static const size_t usernamePasswordsSize = 2;
-static UA_UsernamePasswordLogin usernamePasswords[] = {
-    {UA_STRING_STATIC("user1"), UA_STRING_STATIC("password")},
-    {UA_STRING_STATIC("user2"), UA_STRING_STATIC("password1")}};
+static const size_t usernamePasswordsSize = 1;
+static UA_UsernamePasswordLogin usernamePasswords[] = {{UA_STRING_STATIC("Morfeas"), UA_STRING_STATIC("Morfeas_password")}};
 
 UA_StatusCode Morfeas_OPC_UA_config(UA_ServerConfig *config)
 {
@@ -46,7 +44,7 @@ UA_StatusCode Morfeas_OPC_UA_config(UA_ServerConfig *config)
     config->applicationDescription.applicationUri = UA_STRING_ALLOC("urn:Morfeas.open62541.server.application");
     config->buildInfo.manufacturerName = UA_STRING_ALLOC("Sam-Harry-Tzavaras");
     config->buildInfo.productName = UA_STRING_ALLOC("Morfeas-OPC_UA Server (Based on Open62541)");
-	config->applicationDescription.applicationName = UA_LOCALIZEDTEXT_ALLOC("en", "Morfeas-OPC_UA");
+	config->applicationDescription.applicationName = UA_LOCALIZEDTEXT_ALLOC("en", "Morfeas default application");
 
     retval = UA_ServerConfig_addNetworkLayerTCP(config, 4840, 0, 0);
     if(retval != UA_STATUSCODE_GOOD) {
