384751f4dc0e90c95167876267fb54ddd1d3b55e
Sam Harry Tzavaras <fantomsam@hotmail.com>
2020-01-03 23:44:07 +0200

    Update, Docs

--- src/Morfeas_XML.h
+++ src/Morfeas_XML.h
@@ -21,12 +21,17 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 
-//*** All the function Returns: EXIT_SUCCESS on success, or EXIT_FAILURE or failure. ***//
+//*** All the function Returns: EXIT_SUCCESS on success, or EXIT_FAILURE or failure. Except of other notice ***//
 
+//-- General XML Functions --//
 //Function that parser and validate the Nodeset configuration XML
 int Morfeas_XML_parsing(const char *filename, xmlDocPtr *doc);
-int Morfeas_opc_ua_config_valid(xmlNode *root_element);
+//Function where scan a root xmlNodePtr for node with node_name, return the contents of the node, or NULL if it does not exist
 char * XML_node_get_content(xmlNode *node, const char *node_name);
+
+//-- Morfeas_OPC_UA's Configuration XML related --//
+//Function that validate the content of the Configuration for Morfeas OPC_UA
+int Morfeas_opc_ua_config_valid(xmlNode *root_element);
 //Build list diff with content the ISO_Channels that will be removed
 int Morfeas_OPC_UA_calc_diff_of_ISO_Channel_node(xmlNode *root_element, GSList **cur_Links);
 //Clean all elements of List "cur_ISOChannels" with Re-Build it with data from xmlDoc doc
