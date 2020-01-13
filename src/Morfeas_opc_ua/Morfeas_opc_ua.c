6a66e905986644a1f90df15c32f580c9c5502215
Sam Harry Tzavaras <fantomsam@hotmail.com>
2020-01-13 16:42:18 +0200

    Update

--- src/Morfeas_opc_ua/Morfeas_opc_ua.c
+++ src/Morfeas_opc_ua/Morfeas_opc_ua.c
@@ -256,8 +256,7 @@ void* IPC_Receiver(void *varg_pt)
 			{
 				//--- Message type from any handler (Registration to OPC_UA) ---//
 				case IPC_Handler_register:
-					UA_LOG_INFO(UA_Log_Stdout, 
-								UA_LOGCATEGORY_SERVER, 
+					UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, 
 								"Register %s Handler for %s", Morfeas_IPC_handler_type_name[IPC_msg_dec.Handler_reg.handler_type],
 															  IPC_msg_dec.Handler_reg.connected_to_BUS);
 					switch(IPC_msg_dec.Handler_reg.handler_type)
@@ -274,8 +273,7 @@ void* IPC_Receiver(void *varg_pt)
 					}
 					break;
 				case IPC_Handler_unregister:
-					UA_LOG_INFO(UA_Log_Stdout, 
-								UA_LOGCATEGORY_SERVER, 
+					UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, 
 								"Remove %s Handler for %s",	Morfeas_IPC_handler_type_name[IPC_msg_dec.Handler_reg.handler_type],
 															IPC_msg_dec.Handler_reg.connected_to_BUS);
 					pthread_mutex_lock(&OPC_UA_NODESET_access);
