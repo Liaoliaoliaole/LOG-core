09a442489e30a262a9c7dd8fab9b5b8099cecc36
Sam Harry Tzavaras <fantomsam@hotmail.com>
2021-04-21 21:14:44 +0300

    Update

--- src/Morfeas_NOX/Morfeas_NOX_if.c
+++ src/Morfeas_NOX/Morfeas_NOX_if.c
@@ -418,9 +418,9 @@ int main(int argc, char *argv[])
 				{
 					if(!get_port_meas(&port_meas, stats.port, i2c_bus_num))
 					{
-						stats.Bus_voltage = roundf(100.0 * (port_meas.port_voltage - port_meas_config.volt_meas_offset) * port_meas_config.volt_meas_scaler)/100.0;
-						stats.Bus_amperage = roundf(1000.0 * (port_meas.port_current - port_meas_config.curr_meas_offset) * port_meas_config.curr_meas_scaler)/1000.0;
-						stats.Shunt_temp = roundf(10.0 * port_meas.temperature * MAX9611_temp_scaler)/10.0;
+						stats.Bus_voltage = (port_meas.port_voltage - port_meas_config.volt_meas_offset) * port_meas_config.volt_meas_scaler;
+						stats.Bus_amperage = (port_meas.port_current - port_meas_config.curr_meas_offset) * port_meas_config.curr_meas_scaler;
+						stats.Shunt_temp = port_meas.temperature * MAX9611_temp_scaler;
 					}
 					IPC_msg.NOX_BUS_info.Electrics = -1;
 					IPC_msg.NOX_BUS_info.voltage = stats.Bus_voltage;
