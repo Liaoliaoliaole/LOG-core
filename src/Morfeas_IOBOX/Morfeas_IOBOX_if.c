46633c3975270e5cb034e16e2b6ac3d6b2ec4b59
fantomsam <fantomsam@hotmail.com>
2023-06-29 14:59:32 +0300

    Update, change release version of Morfeas_IOBOX_if

--- src/Morfeas_IOBOX/Morfeas_IOBOX_if.c
+++ src/Morfeas_IOBOX/Morfeas_IOBOX_if.c
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-#define VERSION "1.0" /*Release Version of Morfeas_IOBOX_if*/
+#define VERSION "1.1" /*Release Version of Morfeas_IOBOX_if*/
 
 #include <stdio.h>
 #include <stdlib.h>
