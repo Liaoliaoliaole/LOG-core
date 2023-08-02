c31920bf295b2bf0d60af397c3bfd755095cb503
Sam Harry Tzavaras <fantomsam@hotmail.com>
2023-08-02 16:08:37 +0300

    Update, add headers

--- src/Morfeas_opc_ua/Morfeas_handlers_nodeset.h
+++ src/Morfeas_opc_ua/Morfeas_handlers_nodeset.h
@@ -16,6 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+#include <stdio.h>
 
 #include <open62541/plugin/log_stdout.h>
 #include <open62541/server.h>
